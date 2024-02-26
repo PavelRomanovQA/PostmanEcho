b39597bb79f7de0d0e3aa3b42f00129093f416a9
Pavel <pavel.0gu@.com>
2024-02-26 16:58:46 +0700

    Failed Test

---
+++ src/test/java/ru/netology/rest/PostmanEchoTest.java
@@ -0,0 +1,31 @@
+package ru.netology.rest;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+
+    @Test
+    void shouldReturnSenData() {
+        // Given - When - Then
+        // Предусловия
+        given()
+                .baseUri("https://postman-echo.com")
+                .body("Hello") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                // Выполняемые действия
+                .when()
+                .post("/post")
+                // Проверки
+                .then()
+                .statusCode(200)
+                .contentType(ContentType.JSON)
+                .body("data", equalTo("some data"))
+        ;
+    }
+
+}
